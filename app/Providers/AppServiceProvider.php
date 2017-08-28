b6b20e380aa8dd4920c08f6b65478b7810f83bfb
Vladyslav Startsev <vladyslavstartsev@gmail.com>
2017-08-28 21:42:41 +0300

    initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+            AliasLoader::getInstance(['Debugbar' => \Barryvdh\Debugbar\Facade::class]);
+        }
+    }
+}
